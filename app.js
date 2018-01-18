44cdb774d215f85a3e3bab9482435f2a600c949c
Christian <christian_hane@aol.com>
2018-01-17 23:21:46 -0800

    slj

--- app.js
+++ app.js
@@ -115,6 +115,28 @@ $(document).ready(function(){
             // ...
         });
     });
+
+    firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          var token = result.credential.accessToken;
+          // ...
+        }
+        // The signed-in user info.
+        console.log(result);
+        var user = result.user;
+        console.log(user);
+        console.log(user.displayName);
+        }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
 });
 
 
