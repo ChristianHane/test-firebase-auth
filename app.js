d1e7b9c437044db240a26ea2aaed6920110ebf25
Christian <christian_hane@aol.com>
2018-01-17 22:55:54 -0800

    check fb

--- app.js
+++ app.js
@@ -62,27 +62,17 @@ $(document).ready(function(){
     });
 
 
-    // facebook login
-    var provider = new firebase.auth.GoogleAuthProvider();
-    // firebase.auth().signInWithPopup(provider).then(function(result) {
-    //     // This gives you a Google Access Token. You can use it to access the Google API.
-    //     var token = result.credential.accessToken;
-    //     // The signed-in user info.
-    //     var user = result.user;
-    //     // ...
-    // }).catch(function(error) {
-    //     // Handle Errors here.
-    //     var errorCode = error.code;
-    //     var errorMessage = error.message;
-    //     // The email of the user's account used.
-    //     var email = error.email;
-    //     // The firebase.auth.AuthCredential type that was used.
-    //     var credential = error.credential;
-    //     // ...
-    // });
+    // google login
     $("#google").on("click", function() {
+        var provider = new firebase.auth.GoogleAuthProvider();        
         firebase.auth().signInWithRedirect(provider);    
     })
+
+    //facebook sign in
+    $("#facebook").on("click", function() {
+        var provider = new firebase.auth.FacebookAuthProvider();
+        firebase.auth().signInWithRedirect(provider);
+    });
 });
 
 
