f2e3a857b6ba9061b791f329490c03e75979828c
Christian <christian_hane@aol.com>
2018-01-17 22:36:48 -0800

    check

--- app.js
+++ app.js
@@ -1,36 +1,90 @@
-$("#submit").on("click", function(){
-    var newUser = {
-        firstName: $("#first-name").val(),
-        lastName: $("#last-name").val(),
-        email: $("#email").val(),
-        password: $("#password").val(),
-        repeatPassword: $("repeat-password").val()
-    }
-    if(newUser.password === newUser.repeatPassword){
-        firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password).catch(function(error) {
+$(document).ready(function(){
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBSRyLZcUwZCqIcBvv3GP2ltQ7RA_uQRTU",
+        authDomain: "test-for-auth-78f1a.firebaseapp.com",
+        databaseURL: "https://test-for-auth-78f1a.firebaseio.com",
+        projectId: "test-for-auth-78f1a",
+        storageBucket: "",
+        messagingSenderId: "459604740117"
+    };
+    firebase.initializeApp(config);
+
+    $("#submit").on("click", function(event){
+        event.preventDefault();
+        var email= $("#email").val();
+        var password= $("#password").val();
+        var repeatPassword = $("#repeat-password").val();
+        if(password === repeatPassword){
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(function(user){
+                console.log(user);
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+        }
+    })
+    
+    $("#sign-in").on("click", function(){
+        event.preventDefault();    
+        var email = $("#sign-in-email").val();
+        var password = $("#sign-in-password").val();
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(function(user){
+            // console.log(user);
+        })
+        .catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
+            console.log(error);
+            console.log(errorCode);
             // ...
         });
-    }
-})
-
-$("#sign-in").on("click", function(){
-    var email = $("#sign-in-email").val();
-    var password = $("#sign-in-password").val();
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
     });
-})
-
-$("#sign-out").on("click", function(){
-    firebase.auth().signOut().then(function() {
-        // Sign-out successful.
-      }).catch(function(error) {
-        // An error happened.
+    
+    $("#sign-out").on("click", function(){
+        event.preventDefault();    
+        firebase.auth().signOut().then(function() {
+            // console.log("signed out!");
+          }).catch(function(error) {
+            console.log("something happened with sign out.");
+        });
     });
-})
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if(user) {
+            console.log(user);
+        } else {
+            console.log("no user!");
+        }
+    });
+
+
+    // facebook login
+    var provider = new firebase.auth.GoogleAuthProvider();
+    // firebase.auth().signInWithPopup(provider).then(function(result) {
+    //     // This gives you a Google Access Token. You can use it to access the Google API.
+    //     var token = result.credential.accessToken;
+    //     // The signed-in user info.
+    //     var user = result.user;
+    //     // ...
+    // }).catch(function(error) {
+    //     // Handle Errors here.
+    //     var errorCode = error.code;
+    //     var errorMessage = error.message;
+    //     // The email of the user's account used.
+    //     var email = error.email;
+    //     // The firebase.auth.AuthCredential type that was used.
+    //     var credential = error.credential;
+    //     // ...
+    // });
+    $("#google").on("click", function() {
+        firebase.auth().signInWithRedirect(provider);    
+    })
+});
+
+
+
+
